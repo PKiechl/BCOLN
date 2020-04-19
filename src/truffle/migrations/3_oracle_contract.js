aae24f01cfb60244bc44dfa097a968abe3c77178
fabiank0 <kuefferfabian@gmail.com>
2020-04-19 17:04:14 +0200

    possible fix for contract deployment.

--- src/truffle/migrations/3_oracle_contract.js
+++ src/truffle/migrations/3_oracle_contract.js
@@ -1,7 +1,14 @@
 var MyContract = artifacts.require("Oracle");
+var RouletteContract = artifacts.require("./roulette.sol");
 
-module.exports = function(deployer) {
-  // deployment steps
-  //
-  deployer.deploy(MyContract);
+// module.exports = function(deployer) {
+//   // deployment steps
+//   //
+//   deployer.deploy(MyContract);
+// };
+
+module.exports = (deployer, network) =>{
+  deployer.deploy(MyContract).then(function(){
+    return deployer.deploy(RouletteContract, MyContract.address)
+  });
 };
--- src/truffle/migrations/4_roulette_contract.js
+++
@@ -1,7 +0,0 @@
-var MyContract = artifacts.require("roulette");
-
-module.exports = function(deployer) {
-  // deployment steps
-  //
-  deployer.deploy(MyContract);
-};
