652acd5ecec42ae30f965dd914a278346e104bc8
fabiank0 <kuefferfabian@gmail.com>
2020-05-14 23:31:34 +0200

    Last minute fixes

--- src/components/service/Service.js
+++ src/components/service/Service.js
@@ -1,5 +1,5 @@
 import Web3 from "web3";
-import data from "../../truffle/build/contracts/roulette";
+import data from "../../truffle/build/contracts/Roulette";
 import oracleData from "../../truffle/build/contracts/Oracle";
 
 /*
